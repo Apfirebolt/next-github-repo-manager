8121fb972ba89eb0d2ef3ef8a6a17cee9f22c1e6
Apfirebolt <aspper20@gmail.com>
2025-03-07 21:55:55 +0530

    added auth pages and slices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-brown text-white py-4">
             <div className="container mx-auto text-center">
-                <p>&copy; 2025 Next Games. All rights reserved.</p>
+                <p>&copy; 2025 Next Github Manager. All rights reserved.</p>
             </div>
         </footer>
     );
